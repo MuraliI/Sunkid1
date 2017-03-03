8d4872e1f805a41290fe5fb86f62653716510595
julio.catano <julio.catano@globant.com>
2017-03-03 13:47:04 -0500

    Discover Detail View | Dining (Android) | Title and Text module

--- excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/factory/DinningDetailModuleFactory.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/factory/DinningDetailModuleFactory.java
@@ -1,27 +1,42 @@
 package com.rcl.excalibur.adapters.delegate.factory;
 
 
+import android.content.res.Resources;
 import android.support.v4.util.SparseArrayCompat;
 
+import com.rcl.excalibur.R;
 import com.rcl.excalibur.adapters.base.DelegateAdapter;
+import com.rcl.excalibur.adapters.base.RecyclerViewConstants;
 import com.rcl.excalibur.adapters.base.RecyclerViewType;
+import com.rcl.excalibur.adapters.delegate.TitleAndDescriptionDelegateAdapter;
+import com.rcl.excalibur.adapters.viewtype.TitleAndDescriptionViewType;
+import com.rcl.excalibur.model.DiscoverItemModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DinningDetailModuleFactory implements DetailModuleFactory {
-    private static final int VIEW_TYPES_COUNT = 10;
+    private static final int VIEW_TYPES_COUNT = 1;
+    private DiscoverItemModel itemModel;
 
-    @Override
-    public SparseArrayCompat<DelegateAdapter> getDelegateAdapterArray() {
-        /*FIXME this will return the Array of Delegate adapters that will process the different view types (modules) that the detail
-        will have*/
-        return new SparseArrayCompat<>(VIEW_TYPES_COUNT);
+    public DinningDetailModuleFactory(DiscoverItemModel itemModel) {
+        this.itemModel = itemModel;
     }
 
     @Override
-    public List<RecyclerViewType> getListOfDetailViewTypes() {
-        //FIXME this will return the list of view types (modules) that the detail view must render
-        return new ArrayList<>();
+    public SparseArrayCompat<DelegateAdapter> getDelegateAdapterArray() {
+        SparseArrayCompat<DelegateAdapter> delegateAdapters = new SparseArrayCompat<>(VIEW_TYPES_COUNT);
+        delegateAdapters.append(RecyclerViewConstants.VIEW_TYPE_TITLE_AND_DESCRIPTION, new TitleAndDescriptionDelegateAdapter());
+        return delegateAdapters;
+    }
+
+    @Override
+    public List<RecyclerViewType> getListOfDetailViewTypes(Resources resources) {
+        //TODO get age restriction from model
+        List<RecyclerViewType> viewTypes = new ArrayList<>();
+        viewTypes.add(new TitleAndDescriptionViewType(
+                resources.getString(R.string.discover_item_detail_age_restriction_title),
+                "All ages")); //FIXME get this attributes from model
+        return viewTypes;
     }
 }
