5d8483eb5416230ffd17839d6ce17f5b03167080
Fernando Sierra <jesus.sierra@globant.com>
2017-03-07 15:49:56 -0600

    JIRA: DSC-138 Discover Detail View | Dinning times
    Pull request's comments fixed.
    
    * Vector's dimensions were extracted.
    * Compound drawables were replaced by nested TextView and ImageView in a  horizontal LinearLayout.
    * Menu clickable items' were linked to the click behavior through ButterKnife.
    * RCViewUtils were deleted.
    
    Testing Done:
    Signed-off-by: Fernando Sierra  <jesus.sierra@globant.com>

--- excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/DinnerTimesDelegateAdapter.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/DinnerTimesDelegateAdapter.java
@@ -1,6 +1,5 @@
 package com.rcl.excalibur.adapters.delegate;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,14 +9,12 @@ import android.widget.TextView;
 import com.rcl.excalibur.R;
 import com.rcl.excalibur.adapters.base.DelegateAdapter;
 import com.rcl.excalibur.adapters.viewtype.DinnerTimesViewType;
-import com.rcl.excalibur.utils.RCViewUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class DinnerTimesDelegateAdapter implements DelegateAdapter<
-        DinnerTimesDelegateAdapter.TimesViewHolder,
-        DinnerTimesViewType> {
+public class DinnerTimesDelegateAdapter implements DelegateAdapter<DinnerTimesDelegateAdapter.TimesViewHolder, DinnerTimesViewType> {
 
     @Override
     public TimesViewHolder onCreateViewHolder(ViewGroup parent) {
@@ -28,25 +25,27 @@ public class DinnerTimesDelegateAdapter implements DelegateAdapter<
     public void onBindViewHolder(TimesViewHolder holder, DinnerTimesViewType item) {
         holder.lunchTime.setText(item.getLunchTime());
         holder.dinnerTime.setText(item.getDinnerTime());
-        // TODO: 7/03/17 Trigger intent to read the PDF
-        holder.lunchMenu.setOnClickListener((view) -> Log.d(getClass().getSimpleName(), "Lunch menu opened"));
-        holder.dinnerMenu.setOnClickListener((view) -> Log.d(getClass().getSimpleName(), "Dinner menu opened"));
     }
 
     static class TimesViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.text_module_lunch_time) TextView lunchTime;
-        @Bind(R.id.text_module_lunch_menu) TextView lunchMenu;
         @Bind(R.id.text_module_dinner_time) TextView dinnerTime;
-        @Bind(R.id.text_module_dinner_menu) TextView dinnerMenu;
 
         TimesViewHolder(ViewGroup parent) {
-            super(LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.module_item_detail_dinner_times, parent, false));
+            super(LayoutInflater.from(parent.getContext()).inflate(R.layout.module_item_detail_dinner_times, parent, false));
             ButterKnife.bind(this, itemView);
+        }
 
-            Drawable arrow = RCViewUtils.tintDrawable(parent.getContext(), R.drawable.ic_arrow_drop_down, R.color.detail_item_link);
-            lunchMenu.setCompoundDrawablesWithIntrinsicBounds(null, null, arrow, null);
-            dinnerMenu.setCompoundDrawablesWithIntrinsicBounds(null, null, arrow, null);
+        @OnClick(R.id.discover_detail_lunch_menu)
+        void clickLunchMenu() {
+            // TODO: 7/03/17 Change it to launch the intent to open the PDF
+            Log.d(getClass().getSimpleName(), "Lunch menu opened");
+        }
+
+        @OnClick(R.id.discover_detail_dinner_menu)
+        void clickDinnerMenu() {
+            // TODO: 7/03/17 Change it to launch the intent to open the PDF
+            Log.d(getClass().getSimpleName(), "Dinner menu opened");
         }
     }
 }
