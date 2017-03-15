fa79e705fda3212f7e9b9f92598d1f8a33c29aeb
GLOBANT\gerardo.suarez <gerardo.suarez@globant.com>
2017-03-15 13:44:18 -0500

    Item product with styles and dimens created. ProductModel updated.

--- excalibur/presentation/src/main/java/com/rcl/excalibur/model/ProductModel.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/model/ProductModel.java
@@ -1,4 +1,64 @@
 package com.rcl.excalibur.model;
 
 public class ProductModel {
+    public static final String LOCATION_POINTER_FWD = "Forward";
+    public static final String LOCATION_POINTER_AFT = "After";
+    public static final String LOCATION_POINTER_MID = "Middle";
+
+    private String imageUrl;
+    private String name;
+    private String venue;
+    private String deckNumber;
+    private String locationPointer;
+    private String date;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public String getDeckNumber() {
+        return deckNumber;
+    }
+
+    public String getLocationPointer() {
+        return locationPointer;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public void setDeckNumber(String deckNumber) {
+        this.deckNumber = deckNumber;
+    }
+
+    public void setLocationPointer(String locationPointer) {
+        this.locationPointer = locationPointer;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+
+        return date;
+    }
 }
+
