4a8ff41f8ede05ac314f815229ac5f7b230f53de
julio.catano <julio.catano@globant.com>
2017-03-22 08:53:53 -0500

    Avoided NPE due to triptych fragments being passed through constructor

--- excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/TriptychPagerAdapter.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/TriptychPagerAdapter.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,7 +14,8 @@ public class TriptychPagerAdapter extends FragmentStatePagerAdapter {
 
     public TriptychPagerAdapter(FragmentManager fm, List<Fragment> triptychFragments) {
         super(fm);
-        this.triptychFragments = triptychFragments;
+        this.triptychFragments = new ArrayList<>();
+        this.triptychFragments.addAll(triptychFragments);
     }
 
     @Override
