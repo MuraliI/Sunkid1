288d1894ae9482d1675f662a7308fd9fe6cc3f17
asiel.gil <asiel.gil@globant.com>
2017-03-27 12:06:16 -0400

    Fixing Static fields to Public SimpleDateFormat

--- excalibur/data/src/main/java/com/rcl/excalibur/data/utils/DateUtil.java
+++ excalibur/data/src/main/java/com/rcl/excalibur/data/utils/DateUtil.java
@@ -9,15 +9,14 @@ import java.util.Locale;
 
 public final class DateUtil {
 
-    private static final String DATE_FORMAT = "yyyyMMddHHmm";
-    public static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT, Locale.US);
+    public static final String DATE_FORMAT = "yyyyMMddHHmm";
 
     private DateUtil() {
     }
 
     @Nullable
     public static Date parseDateResponse(String dateStr, String timeStr) {
-
+        SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT, Locale.US);
         Date date = null;
         String str = dateStr + timeStr;
         try {
@@ -29,6 +28,7 @@ public final class DateUtil {
     }
 
     public static String formatDate(Date date) {
+        SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT, Locale.US);
         return DATE_FORMATTER.format(date);
     }
 }
