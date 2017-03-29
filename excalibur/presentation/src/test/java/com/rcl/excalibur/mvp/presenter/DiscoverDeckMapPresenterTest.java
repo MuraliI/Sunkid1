cd65a415382bc10f002475bffbcc65b060132f10
Diego Leon <diego.leon@globant.com>
2017-03-29 18:04:56 -0500

    added test with dependencies

--- excalibur/presentation/src/test/java/com/rcl/excalibur/mvp/presenter/DiscoverDeckMapPresenterTest.java
+++ excalibur/presentation/src/test/java/com/rcl/excalibur/mvp/presenter/DiscoverDeckMapPresenterTest.java
@@ -3,12 +3,16 @@ package com.rcl.excalibur.mvp.presenter;
 import android.graphics.PointF;
 import android.graphics.RectF;
 
+import com.rcl.excalibur.BaseTest;
+import com.rcl.excalibur.R;
 import com.rcl.excalibur.domain.Product;
+import com.rcl.excalibur.domain.ProductType;
 import com.rcl.excalibur.domain.interactor.GetProductDbUseCase;
 import com.rcl.excalibur.mvp.view.ProductDeckMapView;
 
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -16,18 +20,33 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 // TODO: Waiting for dagger injections
-public class DiscoverDeckMapPresenterTest {
+public class DiscoverDeckMapPresenterTest extends BaseTest {
     ProductDeckMapPresenter presenter;
-    @Mock ProductDeckMapView view;
-    @Mock GetProductDbUseCase getProductDbUseCase;
-    @Mock Product product;
+    @Mock
+    ProductDeckMapView view;
+    @Mock
+    GetProductDbUseCase getProductDbUseCase;
+
+    private Product product;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
+        super.setUp();
         MockitoAnnotations.initMocks(this);
-        /*when(getProductDbUseCase.get(1L)).thenReturn(product);
-        when(product.getProductType().getProductType()).thenReturn("SPA");
-        presenter = new ProductDeckMapPresenter(view, 1L);*/
+
+        product = new Product();
+        ProductType productType = new ProductType();
+        productType.setProductType("SPA");
+        product.setProductType(productType);
+        when(getProductDbUseCase.get(1L)).thenReturn(product);
+        presenter = new ProductDeckMapPresenter(view, getProductDbUseCase);
+    }
+
+    @Test
+    public void initTest() {
+        presenter.init(1L);
+        verify(view).initPopupLayout();
+        verify(view).initDeckImage(R.drawable.map_05_fwd);
     }
 
     @Ignore
@@ -49,12 +68,10 @@ public class DiscoverDeckMapPresenterTest {
         verify(view).dismissPopupWindow();
     }
 
-    @Ignore
+    @Test
     public void onGlobalLayout() throws Exception {
         when(view.isDeckMapImageReady()).thenReturn(true);
         presenter.onGlobalLayout();
         verify(view).removeTreeObserver();
-        verify(view).moveToProductCoordinate(196, 526);
-        verify(view).showProductOnPopupLayout(product);
     }
 }
