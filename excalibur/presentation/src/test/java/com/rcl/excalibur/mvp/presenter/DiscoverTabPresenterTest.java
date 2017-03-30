05fe132a97fbe14814f9ee367a7dc40e50b99e25
norberto.lopez <norberton86@gmail.com>
2017-03-30 13:56:50 -0500

    Improve Coverage 8

--- excalibur/presentation/src/test/java/com/rcl/excalibur/mvp/presenter/DiscoverTabPresenterTest.java
+++ excalibur/presentation/src/test/java/com/rcl/excalibur/mvp/presenter/DiscoverTabPresenterTest.java
@@ -1,44 +1,51 @@
 package com.rcl.excalibur.mvp.presenter;
 
-import com.rcl.excalibur.ProductsBaseTest;
+import com.rcl.excalibur.internal.di.component.AppComponentTest;
+import com.rcl.excalibur.internal.di.component.DaggerAppComponentTest;
+import com.rcl.excalibur.internal.di.component.products.ProductsComponentTest;
 import com.rcl.excalibur.internal.di.component.products.ProductsFragmentComponentTest;
+import com.rcl.excalibur.internal.di.module.AppModuleTest;
+import com.rcl.excalibur.internal.di.module.products.ProductsDatabaseModuleTest;
 import com.rcl.excalibur.internal.di.module.products.ProductsFragmentModuleTest;
+import com.rcl.excalibur.internal.di.module.products.ProductsServicesModuleTest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.inject.Inject;
 
 import static org.mockito.Mockito.verify;
 
-
-public class DiscoverTabPresenterTest extends ProductsBaseTest {
+public class DiscoverTabPresenterTest {
 
     @Inject DiscoverTabPresenter presenter;
+    ProductsComponentTest productsComponentTest;
     ProductsFragmentComponentTest productsFragmentComponentTest;
+    AppComponentTest appComponentTest;
 
-
-    @Override
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
-        productsFragmentComponentTest = productsComponent.plus(new ProductsFragmentModuleTest());
+
+        appComponentTest = DaggerAppComponentTest.builder().appModule(new AppModuleTest()).build();
+        productsComponentTest = appComponentTest.plus(new ProductsServicesModuleTest(), new ProductsDatabaseModuleTest());
+        productsFragmentComponentTest = productsComponentTest.plus(new ProductsFragmentModuleTest());
         productsFragmentComponentTest.inject(this);
     }
 
-
-    @Override
+    @After
     public void tearDown() throws Exception {
-        super.tearDown();
+        appComponentTest = null;
+        productsComponentTest = null;
         productsFragmentComponentTest = null;
     }
 
     @Test
     public void openListScreen() {
-
         int fragmentToShow = 0;
         presenter.openListScreen(fragmentToShow);
 
         verify(presenter.getView()).openListScreen(fragmentToShow);
-
     }
 
 }
