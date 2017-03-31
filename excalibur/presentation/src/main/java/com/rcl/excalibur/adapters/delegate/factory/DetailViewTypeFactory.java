29334aa1dea7583e0d75f089c38ea7f75cf57afe
ads.f117 <ads.f117@gmail.com>
2017-03-31 16:38:43 -0500

    revert addAdvisments metod

--- excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/factory/DetailViewTypeFactory.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/factory/DetailViewTypeFactory.java
@@ -114,14 +114,12 @@ public final class DetailViewTypeFactory {
                 productActivityLevel.getActivityLevelTitle());
     }
 
-    private static void addAdvisements(final List<RecyclerViewType> recyclerViewTypeList, @NonNull Resources resources, ProductModel product) {
+    private static void addAdvisements(final List<RecyclerViewType> recyclerViewTypeList, @NonNull Resources resources,
+                                       ProductModel product) {
         // FIXME: Obtain products from database
         product = ProductModelProvider.productModelMap.get("1");
-
         LinkedHashMap<String, String> advisements = product.getAdvisements();
-        if (advisements.size() == 0) {
-            return;
-        }
+
         for (Map.Entry<String, String> entry : advisements.entrySet()) {
             String advisementTitle = entry.getKey();
             String advisementDescription = entry.getValue();
