9e6a73d118a7896edd06a07a56dc81bb98c2f6e4
GLOBANT\diego.recalde <diego.recalde@globant.com>
2017-04-04 17:37:16 -0500

    Modified animation parallax, color and title to match the requirements.

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/ProductDetailView.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/ProductDetailView.java
@@ -1,5 +1,6 @@
 package com.rcl.excalibur.mvp.view;
 
+import android.support.annotation.ColorRes;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
@@ -8,7 +9,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mmin18.widget.RealtimeBlurView;
@@ -16,6 +19,7 @@ import com.rcl.excalibur.R;
 import com.rcl.excalibur.activity.ProductDetailActivity;
 import com.rcl.excalibur.adapters.base.RecyclerViewType;
 import com.rcl.excalibur.adapters.delegate.DetailViewCoordinatorAdapter;
+import com.rcl.excalibur.domain.utils.ConstantsUtil;
 import com.rcl.excalibur.mvp.view.base.ActivityView;
 import com.squareup.picasso.Picasso;
 
@@ -26,7 +30,6 @@ import butterknife.ButterKnife;
 
 
 public class ProductDetailView extends ActivityView<ProductDetailActivity> {
-    private static final int MAX_BLUR_VALUE = 25;
 
     @Bind(R.id.recycler_discover_item_details) RecyclerView planDetailRecycler;
     @Bind(R.id.app_bar_layout_detail) AppBarLayout appBarLayout;
@@ -64,18 +67,46 @@ public class ProductDetailView extends ActivityView<ProductDetailActivity> {
     }
 
     public void setDetailTitle(String title) {
-        collapsingToolbar.setTitle(title);
+        //collapsingToolbar.setTitle(title);
     }
+
+    private int scrolledAmount = 0;
 
     public void render(List<RecyclerViewType> viewTypes) {
         adapter = new DetailViewCoordinatorAdapter(viewObserver, viewTypes);
         planDetailRecycler.setAdapter(adapter);
 
-        if (getActivity() != null) {
-            LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-            planDetailRecycler.setLayoutManager(layoutManager);
-            planDetailRecycler.addItemDecoration(new DividerItemDecoration(getActivity(), layoutManager.getOrientation()));
+        ProductDetailActivity activity = getActivity();
+        if (activity == null) {
+            return;
         }
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
+        planDetailRecycler.setLayoutManager(layoutManager);
+        planDetailRecycler.addItemDecoration(new DividerItemDecoration(activity, layoutManager.getOrientation()));
+        // Cannot be passed to Activity because RecyclerView.OnScrollListener is a class and not an interface
+        planDetailRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                scrolledAmount += dy;
+
+                if (layoutManager.findFirstVisibleItemPosition() == 0) {
+                    View view = recyclerView.getChildAt(0);
+                    if (view == null) {
+                        return;
+                    }
+
+                    TextView textView = (TextView) view.findViewById(R.id.text_module_title);
+                    if (scrolledAmount >= view.getPaddingTop() + (textView.getHeight() / 2)) {
+                        collapsingToolbar.setTitle(textView.getText().toString());
+                    } else {
+                        collapsingToolbar.setTitle(ConstantsUtil.EMPTY);
+                    }
+                }
+            }
+        });
     }
 
     public void showToastAndFinishActivity(String message) {
@@ -85,19 +116,19 @@ public class ProductDetailView extends ActivityView<ProductDetailActivity> {
         }
     }
 
-    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+    public void setBlurRadiusOnImage(float blurRadius) {
         ProductDetailActivity activity = getActivity();
         if (activity == null) {
             return;
         }
         realtimeBlurView.setBlurRadius(TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
-                getBlurRadius(appBarLayout, verticalOffset),
+                blurRadius,
                 activity.getResources().getDisplayMetrics())
         );
     }
 
-    private float getBlurRadius(AppBarLayout appBarLayout, int verticalOffset) {
-        return (Math.abs(verticalOffset) * MAX_BLUR_VALUE) / appBarLayout.getTotalScrollRange();
+    public void setContentScrimResource(@ColorRes int scrimRes) {
+        collapsingToolbar.setContentScrimResource(scrimRes);
     }
 }
