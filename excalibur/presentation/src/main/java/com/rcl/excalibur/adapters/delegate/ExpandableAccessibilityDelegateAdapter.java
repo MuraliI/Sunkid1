cec66eae671b910bec4c833f6fc3d5d10b221eff
GLOBANT\gerardo.suarez <gerardo.suarez@globant.com>
2017-04-04 17:51:34 -0500

    Accessiblity module working on. Pending add line below title

--- excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/ExpandableAccessibilityDelegateAdapter.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/adapters/delegate/ExpandableAccessibilityDelegateAdapter.java
@@ -1,10 +1,6 @@
 package com.rcl.excalibur.adapters.delegate;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -14,22 +10,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.rcl.excalibur.BuildConfig;
 import com.rcl.excalibur.R;
 import com.rcl.excalibur.adapters.base.DelegateAdapter;
 import com.rcl.excalibur.adapters.viewtype.ExpandableAccesibilityViewType;
+import com.rcl.excalibur.model.ProductAccessibilityModel;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import uk.co.chrisjenx.calligraphy.CalligraphyUtils;
-
-import static android.view.View.GONE;
 
 public class ExpandableAccessibilityDelegateAdapter implements
         DelegateAdapter<ExpandableAccessibilityDelegateAdapter.ExpandableAccessibiltyViewHolder, ExpandableAccesibilityViewType> {
@@ -41,101 +33,63 @@ public class ExpandableAccessibilityDelegateAdapter implements
     @Override
     public void onBindViewHolder(ExpandableAccessibiltyViewHolder holder, ExpandableAccesibilityViewType item) {
         holder.title.setText(item.getTitle());
-        fillContentLines(item.getAccessibilities().size(), holder, item);
+        fillContentLines(holder, item.getAccessibilities());
     }
 
-    private static void fillContentLines(int numberOfContentLines, ExpandableAccessibiltyViewHolder viewHolder,
-                                         ExpandableAccesibilityViewType item) {
-        viewHolder.textContentParent.removeAllViews();
-        Resources resources = viewHolder.itemView.getResources();
+    private static void fillContentLines(ExpandableAccessibiltyViewHolder viewHolder,
+                                         List<ProductAccessibilityModel> accessibilityList) {
+        viewHolder.layoutContent.removeAllViews();
         Context context = viewHolder.itemView.getContext();
 
-        for (int i = 0; i < numberOfContentLines; i++) {
-            final TextView subtitleLine = new TextView(context);
-            final TextView descriptionLine = new TextView(context);
+        for (int i = 0; i < accessibilityList.size(); i++) {
 
-            final Target target = new Target() {
-                @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom loadedFrom) {
-                    BitmapDrawable drawable = new BitmapDrawable(context.getResources(), bitmap);
-                    setCompoundDrawable(subtitleLine, drawable, context, resources);
-                }
+            View itemView = LayoutInflater.from(context).inflate(R.layout.accessibility_item,
+                    null);
 
-                @Override
-                public void onBitmapFailed(Drawable drawable) {
-                    setCompoundDrawable(subtitleLine, R.drawable.ic_blue_checkbox, context, resources);
-                }
+            final TextView subtitleLine = (TextView) itemView.findViewById(R.id.text_subtitle);
+            final TextView descriptionLine = (TextView) itemView.findViewById(R.id.text_description);
+            final ImageView image = (ImageView) itemView.findViewById(R.id.image_accessibility);
+            final int iconSize = 10;
 
-                @Override
-                public void onPrepareLoad(Drawable drawable) {
+            String imageUrl = accessibilityList.get(i).getImageUrl();
+            if (!TextUtils.isEmpty(imageUrl)) {
+                Picasso.with(context)
+                        .load(accessibilityList.get(i).getImageUrl())
+                        .resize(iconSize, iconSize)
+                        .placeholder(R.drawable.ic_blue_checkbox)
+                        .into(image);
+            } else {
+                image.setBackgroundResource(R.drawable.ic_blue_checkbox);
+            }
 
-                }
-            };
-
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.topMargin = (int) resources.getDimension(R.dimen.margin_normal);
-
-            String subtitle = item.getAccessibilities().get(i).getSubtitle();
+            String subtitle = accessibilityList.get(i).getSubtitle();
             if (subtitle != null) {
-                subtitleLine.setLayoutParams(params);
                 subtitleLine.setText(subtitle);
                 CalligraphyUtils.applyFontToTextView(context, descriptionLine, context.getResources().getString(R.string.proximanova_regular));
-                viewHolder.textContentParent.addView(subtitleLine);
             }
 
-            String imageUrl = item.getAccessibilities().get(i).getImageUrl();
-            viewHolder.targetList.add(target);
-            final int iconSize = 10;
-            if (TextUtils.isEmpty(imageUrl)) {
-                Picasso.with(context)
-                        .load(BuildConfig.PREFIX_IMAGE + imageUrl)
-                        .resize(iconSize, iconSize)
-                        .into(target);
-            } else {
-                setCompoundDrawable(subtitleLine, R.drawable.ic_blue_checkbox, context, resources);
-            }
-
-            String description = item.getAccessibilities().get(i).getDescription();
+            String description = accessibilityList.get(i).getDescription();
             if (!TextUtils.isEmpty(description)) {
                 descriptionLine.setText(description);
-                params.leftMargin = (int) resources.getDimension(R.dimen.margin_normal);
                 CalligraphyUtils.applyFontToTextView(context, descriptionLine, context.getResources().getString(R.string.proximanova_regular));
-                descriptionLine.setLayoutParams(params);
-                viewHolder.textContentParent.addView(descriptionLine);
             }
+            viewHolder.layoutContent.addView(itemView);
         }
-    }
-
-    private static void setCompoundDrawable(TextView textView, BitmapDrawable drawable, Context context, Resources resources) {
-        textView.setCompoundDrawablesWithIntrinsicBounds(
-                drawable,
-                null,
-                null,
-                null);
-        textView.setCompoundDrawablePadding((int) resources.getDimension(R.dimen.margin_normal));
-    }
-
-    private static void setCompoundDrawable(TextView textView, int drawableResource, Context context, Resources resources) {
-        textView.setCompoundDrawablesWithIntrinsicBounds(
-                resources.getDrawable(R.drawable.ic_blue_checkbox),
-                null,
-                null,
-                null);
-        textView.setCompoundDrawablePadding((int) resources.getDimension(R.dimen.margin_normal));
     }
 
     static class ExpandableAccessibiltyViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.expandable_accessilibility_module_title)
         TextView title;
-        @Bind(R.id.expandable_accessibility_module_content_parent)
-        LinearLayout textContentParent;
-        @Bind(R.id.expandable_accessibility_module_content_child)
-        LinearLayout textContentChild;
+        @Bind(R.id.expandable_link_module_content)
+        LinearLayout layoutContent;
         @Bind(R.id.expandable_accessilibility_module_arrow)
         ImageView imageArrow;
-        public List<Target> targetList = new ArrayList<>();
+
+        ExpandableAccessibiltyViewHolder(ViewGroup parent) {
+            super(LayoutInflater.from(parent.getContext()).inflate(R.layout.module_item_detail_expandable_accessibility,
+                    parent, false));
+            ButterKnife.bind(this, itemView);
+        }
 
         @OnClick(R.id.expandable_accessibility_module_container)
         public void onTitleClick() {
@@ -143,16 +97,9 @@ public class ExpandableAccessibilityDelegateAdapter implements
         }
 
         public void change() {
-            final boolean isGone = GONE == textContentParent.getVisibility();
-            textContentParent.setVisibility(isGone ? View.VISIBLE : GONE);
-            textContentChild.setVisibility(isGone ? View.VISIBLE : GONE);
+            final boolean isGone = View.GONE == layoutContent.getVisibility();
+            layoutContent.setVisibility(isGone ? View.VISIBLE : View.GONE);
             imageArrow.setImageResource(isGone ? R.drawable.ic_arrow_drop_up : R.drawable.ic_arrow_drop_down);
-        }
-
-        ExpandableAccessibiltyViewHolder(ViewGroup parent) {
-            super(LayoutInflater.from(parent.getContext()).inflate(R.layout.module_item_detail_expandable_accessibility,
-                    parent, false));
-            ButterKnife.bind(this, itemView);
         }
     }
 }
