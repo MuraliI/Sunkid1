51c1950d705641a8f4398f70c874c13e5dcf7760
Diego Leon <diego.leon@globant.com>
2017-04-05 06:58:07 -0500

    add password presenter

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/presenter/guest/PasswordPresenter.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/presenter/guest/PasswordPresenter.java
@@ -1,6 +1,12 @@
 package com.rcl.excalibur.mvp.presenter.guest;
 
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+
 import com.rcl.excalibur.R;
 import com.rcl.excalibur.activity.BaseActivity;
 import com.rcl.excalibur.mvp.presenter.ActivityPresenter;
@@ -8,15 +14,12 @@ import com.rcl.excalibur.mvp.view.base.ActivityView;
 import com.rcl.excalibur.mvp.view.guest.PasswordView;
 import com.rcl.excalibur.utils.ActivityUtils;
 
-import static com.rcl.excalibur.utils.ValidateFieldsUtils.isValidateNext;
-import static com.rcl.excalibur.utils.ValidateFieldsUtils.isValidatePassword;
-
 public class PasswordPresenter implements ActivityPresenter {
+    private static final int MIN_CHAR = 7;
     private PasswordView view;
 
     public PasswordPresenter(PasswordView view) {
         this.view = view;
-        init();
     }
 
     public void onHeaderBackOnClick() {
@@ -25,11 +28,6 @@ public class PasswordPresenter implements ActivityPresenter {
             return;
         }
         ActivityUtils.onBackActivity(activity);
-    }
-
-
-    private void init() {
-        view.init();
     }
 
     public void setFocus(boolean hasFocus) {
@@ -46,7 +44,7 @@ public class PasswordPresenter implements ActivityPresenter {
         if (activity == null) {
             return;
         }
-        view.setError(isValidatePassword(view.getPassword(), activity));
+        view.setError(isValidPassword(view.getPassword(), activity));
         view.setNextButton(isValidateNext(view.getPassword()) ? R.drawable.next_active : R.drawable.next,
                 isValidateNext(view.getPassword()));
     }
@@ -61,5 +59,44 @@ public class PasswordPresenter implements ActivityPresenter {
     @Override
     public ActivityView getView() {
         return view;
+    }
+
+    private SpannableStringBuilder isValidPassword(String password, Context context) {
+        boolean hasUppercase = password.equals(password.toLowerCase());
+        boolean hasLowercase = password.equals(password.toUpperCase());
+        boolean isAtLeast7 = password.length() <= MIN_CHAR;
+        boolean hasSpecial = !password.matches(".*[!@#$%^&*].*");
+
+        String minChars = context.getString(R.string.min_characters);
+        String uppercaseChars = context.getString(R.string.must_uppercase);
+        String lowercaseChars = context.getString(R.string.must_lowercase);
+        String specialChars = context.getString(R.string.must_special_character);
+        String whiteSpace = context.getString(R.string.space_string);
+
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(getSpannableString(minChars, isAtLeast7));
+        builder.append(whiteSpace);
+        builder.append(getSpannableString(uppercaseChars, hasUppercase));
+        builder.append(whiteSpace);
+        builder.append(getSpannableString(lowercaseChars, hasLowercase));
+        builder.append(whiteSpace);
+        builder.append(getSpannableString(specialChars, hasSpecial));
+
+        return builder;
+    }
+
+    private boolean isValidateNext(String password) {
+        boolean hasUppercase = password.equals(password.toLowerCase());
+        boolean hasLowercase = password.equals(password.toUpperCase());
+        boolean isAtLeast7 = password.length() <= MIN_CHAR;
+        boolean hasSpecial = !password.matches(".*[$&+,:;=?@#|/'<>.^*()%!-].*");
+
+        return !isAtLeast7 && !hasUppercase && !hasLowercase && !hasSpecial;
+    }
+
+    private static SpannableString getSpannableString(String errorString, boolean isError) {
+        SpannableString spannableString = new SpannableString(errorString);
+        spannableString.setSpan(isError ? new StyleSpan(Typeface.BOLD) : null, 0, spannableString.length(), 0);
+        return spannableString;
     }
 }
