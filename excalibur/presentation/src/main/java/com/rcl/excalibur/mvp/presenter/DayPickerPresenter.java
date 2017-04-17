3180b03d63a4a97a86dca6042d38672ea3f0dbe0
GLOBANT\gerardo.suarez <gerardo.suarez@globant.com>
2017-04-17 18:48:50 -0500

    Partial changes to MVP

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/presenter/DayPickerPresenter.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/presenter/DayPickerPresenter.java
@@ -4,10 +4,10 @@ package com.rcl.excalibur.mvp.presenter;
 import com.rcl.excalibur.fragments.DayPickerFragment;
 import com.rcl.excalibur.mvp.view.base.FragmentView;
 
-public class DayPickerPresenter extends FragmentView<DayPickerFragment, Void, Void> {
+public class DayPickerPresenter {
 
     public DayPickerPresenter(DayPickerFragment fragment) {
-        super(fragment);
+
     }
 
 }
--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/DayPickerView.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/DayPickerView.java
@@ -1,11 +1,22 @@
 package com.rcl.excalibur.mvp.view;
 
 
+import android.widget.TextView;
+
+import com.rcl.excalibur.R;
 import com.rcl.excalibur.fragments.DayPickerFragment;
 import com.rcl.excalibur.mvp.view.base.FragmentView;
 
+import butterknife.Bind;
+
 public class DayPickerView extends FragmentView<DayPickerFragment, Void, Void> {
+    @Bind(R.id.day_picker_startandend_date) TextView textDayPicker;
+
     protected DayPickerView(DayPickerFragment fragment) {
         super(fragment);
+    }
+
+    public void setDate(String date) {
+        textDayPicker.setText(date);
     }
 }
