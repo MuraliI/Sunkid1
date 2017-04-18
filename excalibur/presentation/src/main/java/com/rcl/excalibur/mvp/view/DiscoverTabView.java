22587420e401b90554fbe8899277f4d36a2ab72f
itzu.villarce <itzu.villarce@globant.com>
2017-04-18 09:25:48 -0500

    Get category information from db.
    Signed-off-by : Itzu Villarce  <itzu.villarce@globant.com>

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/DiscoverTabView.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/DiscoverTabView.java
@@ -2,20 +2,12 @@ package com.rcl.excalibur.mvp.view;
 
 import com.rcl.excalibur.activity.BaseActivity;
 import com.rcl.excalibur.activity.PlanListActivity;
-import com.rcl.excalibur.data.utils.Preconditions;
 import com.rcl.excalibur.fragments.DiscoverTabFragment;
 import com.rcl.excalibur.mvp.view.base.FragmentView;
 import com.rcl.excalibur.utils.ActivityUtils;
 
 public class DiscoverTabView extends FragmentView<DiscoverTabFragment, Void, Void> {
 
-
-    private String SHOREX = "shorex";
-    private String ACYIVITY = "ACT";
-    private String SHOPPING = "Shopping";
-    private String SPA = "Spa";
-    private String DINING = "dining";
-    private String ENTRETAINMENT = "ent";
 
     public DiscoverTabView(DiscoverTabFragment fragment) {
         super(fragment);
@@ -27,35 +19,8 @@ public class DiscoverTabView extends FragmentView<DiscoverTabFragment, Void, Voi
             return;
         }
 
-        String category = null;
+        ActivityUtils.startActivity(activity, PlanListActivity.getStartIntent(activity, fragmentToShow));
 
-        switch (fragmentToShow) {
-            case PlanListView.POSITION_ROYAL_ACTIVITY:
-                category = ACYIVITY;
-                break;
-            case PlanListView.POSITION_DINING:
-                category = DINING;
-                break;
-            case PlanListView.POSITION_SHOPPING:
-                category = SHOPPING;
-                break;
-            case PlanListView.POSITION_SPA:
-                category = SPA;
-                break;
-            case PlanListView.POSITION_SHOREX:
-                category = SHOREX;
-                break;
-            case PlanListView.POSITION_ENTERTAINMENT:
-                category = ENTRETAINMENT;
-                break;
-            default:
-                Preconditions.unreachable();
-
-        }
-
-        if (category != null) {
-            ActivityUtils.startActivity(activity, PlanListActivity.getStartIntent(activity, fragmentToShow));
-        }
     }
 }
 
