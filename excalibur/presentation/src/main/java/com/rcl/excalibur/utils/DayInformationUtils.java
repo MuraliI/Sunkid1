0e0d84f29aa92c951af96067c800ae5aa704882f
Jockzan Cardona <jockzan.cardona@globant.com>
2017-04-18 17:08:40 -0500

    Fixing DayInformation Logic, Adding DayInformation Test

--- excalibur/presentation/src/main/java/com/rcl/excalibur/utils/DayInformationUtils.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/utils/DayInformationUtils.java
@@ -19,7 +19,7 @@ public final class DayInformationUtils {
     private static final String PORT_TYPE_DOCKED = "DOCKED";
     private static final String PORT_TYPE_DEBARK = "DEBARK";
     private static final String PORT_TYPE_CRUISING = "CRUISING";
-    private static final String PORT_TYPE_AT_SEA = "AT SEA";
+    private static final String PORT_TYPE_AT_SEA = "At Sea";
     private static final int DEFAULT_TIME_VALUE = 0;
     private static final String DATE_FORMAT = "MM/dd/yyyy";
     private static final String ARRIVING_AT = " at ";
@@ -39,7 +39,7 @@ public final class DayInformationUtils {
     }
 
     @Nullable
-    private static String getShipLocation(@NonNull SailPort sailPort) {
+    public static String getShipLocation(@NonNull SailPort sailPort) {
         String shipLocation;
         String modelPortType = sailPort.getPortType();
         if (modelPortType.equalsIgnoreCase(PORT_TYPE_EMBARK)
@@ -88,17 +88,31 @@ public final class DayInformationUtils {
 
     private static String getTimeFormat(@NonNull int arrivalTime) {
         String arrivalTimeString = String.valueOf(arrivalTime);
-
-        StringBuilder time = appendValues(arrivalTimeString.substring(BEGIN_TIME_INDEX, MIDDLE_TIME_INDEX),
-                TIME_FORMAT_SEPARATOR, arrivalTimeString.substring(MIDDLE_TIME_INDEX, END_TIME_INDEX));
-
+        int endString = arrivalTimeString.length() - 1;
+        String am_pm;
+        String timeHour;
+        String timeMinute = getStringTimeData(arrivalTimeString, endString, 0, true);
         if (arrivalTime >= MAX_TIME_HOUR) {
-            time.append(TIME_FORMAT_PM);
+            am_pm = TIME_FORMAT_PM;
+            timeHour = getStringTimeData(arrivalTimeString, endString, 12, false);
         } else {
-            time.append(TIME_FORMAT_AM);
+            am_pm = TIME_FORMAT_AM;
+            timeHour = getStringTimeData(arrivalTimeString, endString, 0, false);
         }
 
+        StringBuilder time = appendValues(timeHour, TIME_FORMAT_SEPARATOR, timeMinute, am_pm);
+
         return time.toString();
+    }
+
+    private static String getStringTimeData(String arrivalTimeString, int endString, int dif, boolean isMinute) {
+        String stringTime;
+        if (isMinute) {
+            stringTime = arrivalTimeString.substring(endString - END_TIME_INDEX, endString - MIDDLE_TIME_INDEX);
+        } else {
+            stringTime = String.valueOf(Integer.getInteger(arrivalTimeString.substring(BEGIN_TIME_INDEX, endString - END_TIME_INDEX)) - dif);
+        }
+        return stringTime;
     }
 
     private static StringBuilder appendValues(@NonNull String... values) {
---
+++ excalibur/presentation/src/test/java/com/rcl/excalibur/utils/DayInformationUtilsTest.java
@@ -0,0 +1,72 @@
+package com.rcl.excalibur.utils;
+
+import com.rcl.excalibur.domain.SailPort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DayInformationUtilsTest {
+
+    private SailPort sailPort;
+
+    @Before
+    public void setUp() throws Exception {
+        sailPort = new SailPort();
+    }
+
+    @Test
+    public void testShipLocationNamePort() throws Exception {
+        initSailPort_withPortData(sailPort, "04/24/2017");
+        String shipLocation = DayInformationUtils.getShipLocation(sailPort);
+        assertEquals(shipLocation, "NASSAU, BAHAMAS");
+    }
+
+    @Test
+    public void testShipLocationAtSea() throws Exception {
+        initSailPort_withAtSeaData(sailPort, "04/25/2017");
+        String shipLocation = DayInformationUtils.getShipLocation(sailPort);
+        assertEquals(shipLocation, "At Sea");
+    }
+
+    @Test
+    public void testArrivalDebarkTime_today() throws Exception {
+        initSailPort_withPortData(sailPort, "04/18/2017");
+        String arrivalDebarkTime = DayInformationUtils.getArrivalDebarkTime(sailPort);
+        assertEquals(arrivalDebarkTime, "Debarking at 7:00am - Embarking at 14:00pm");
+    }
+
+    @Test
+    public void testArrivalDebarkTime_otherDay() throws Exception {
+        initSailPort_withPortData(sailPort, "04/24/2017");
+        String arrivalDebarkTime = DayInformationUtils.getArrivalDebarkTime(sailPort);
+        assertEquals(arrivalDebarkTime, "");
+    }
+
+    @Test
+    public void getTimeFormat() throws Exception {
+
+    }
+
+    private void initSailPort_withPortData(SailPort sailPort, String date) {
+        sailPort.setPortCode("NAS");
+        sailPort.setPortName("NASSAU, BAHAMAS");
+        sailPort.setPortType("DOCKED");
+        sailPort.setArrivalDate(date);
+        sailPort.setDepartureDate("04/24/2017");
+        sailPort.setArrivalTime(70000);
+        sailPort.setDepartureTime(140000);
+    }
+
+    private void initSailPort_withAtSeaData(SailPort sailPort, String date) {
+        sailPort.setPortCode("CRU");
+        sailPort.setPortName("CRUISING");
+        sailPort.setPortType("CRUISING");
+        sailPort.setArrivalDate(date);
+        sailPort.setDepartureDate("04/25/2017");
+        sailPort.setArrivalTime(0);
+        sailPort.setDepartureTime(0);
+    }
+
+}
