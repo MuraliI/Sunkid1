a429dddaa42b77eefe1ce080fc474fd260a2dd11
Itzu Villarce Cervantes <itzu.villarce@globant.com>
2017-04-18 11:01:44 -0500

    Change PlanListView to add information in each category from data base.
    Signed-off-by : Itzu Villarce  <itzu.villarce@globant.com>

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/PlanListView.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/PlanListView.java
@@ -2,14 +2,15 @@ package com.rcl.excalibur.mvp.view;
 
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.widget.TextView;
 
 import com.rcl.excalibur.R;
 import com.rcl.excalibur.activity.PlanListActivity;
 import com.rcl.excalibur.adapters.ProductsCategoryAdapter;
+import com.rcl.excalibur.data.repository.SubCategoriesDataRepository;
 import com.rcl.excalibur.data.utils.Preconditions;
+import com.rcl.excalibur.domain.ChildCategory;
 import com.rcl.excalibur.domain.SubCategory;
 import com.rcl.excalibur.domain.interactor.GetSubCategoryDbUseCase;
 import com.rcl.excalibur.fragments.ProductsListFragment;
@@ -19,11 +20,11 @@ import com.rcl.excalibur.utils.analytics.AnalyticEvent;
 import com.rcl.excalibur.utils.analytics.AnalyticsConstants;
 import com.rcl.excalibur.utils.analytics.AnalyticsUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 import static com.rcl.excalibur.fragments.ProductsListFragment.DINING;
 import static com.rcl.excalibur.fragments.ProductsListFragment.ENTERTAINMENT;
@@ -31,7 +32,6 @@ import static com.rcl.excalibur.fragments.ProductsListFragment.ROYAL_ACTIVITY;
 import static com.rcl.excalibur.fragments.ProductsListFragment.SHOPPING;
 import static com.rcl.excalibur.fragments.ProductsListFragment.SHOREX;
 import static com.rcl.excalibur.fragments.ProductsListFragment.SPA;
-import static com.rcl.excalibur.fragments.ProductsListFragment.newInstance;
 
 
 public class PlanListView extends ActivityView<PlanListActivity, Void, DiscoverItemModel> {
@@ -41,8 +41,16 @@ public class PlanListView extends ActivityView<PlanListActivity, Void, DiscoverI
     public static final int POSITION_SPA = 3;
     public static final int POSITION_SHOREX = 4;
     public static final int POSITION_ENTERTAINMENT = 5;
+
+    private static final String ID_SHOREX = "shorex";
+    private static final String ID_ACYIVITY = "ACT";
+    private static final String ID_SHOPPING = "shop";
+    private static final String ID_SPA = "spa";
+    private static final String ID_DINING = "dining";
+    private static final String ID_ENTRETAINMENT = "ent";
+
     private GetSubCategoryDbUseCase getSubCategoryDbUseCase;
-    private List<SubCategory> subCategory;
+    private SubCategory subCategory;
 
     @Bind(R.id.plans_header_name) TextView plansHeaderName;
     @Bind(R.id.tab_layout_products_list_categories) TabLayout tabProductCategories;
@@ -59,98 +67,68 @@ public class PlanListView extends ActivityView<PlanListActivity, Void, DiscoverI
             return;
         }
 
-        setUpViewPagerProductCategories(fragmentToShow, pagerFragment, getCategoriesDummies(fragmentToShow));
+        createFragment(fragmentToShow, pagerFragment);
         tabProductCategories.setupWithViewPager(pagerFragment, true);
     }
 
-    private void setUpViewPagerProductCategories(int fragmentToShow, ViewPager viewPager, List<String> categories) {
-
-        /*getSubCategoryDbUseCase = new GetSubCategoryDbUseCase(new SubCategoriesDataRepository());
-
-        subCategory = getSubCategoryDbUseCase.getAll();
-
-        Timber.d("Categories " + "Name : " + subCategory.getCategoryName() + " Id : " + subCategory.getCategoryId() +
-                " SubCategory : " + subCategory.getChildCategory() + " Description : " + subCategory.getCategoryDescription());*/
-
-        ProductsCategoryAdapter adapter = new ProductsCategoryAdapter(getFragmentManager());
-        for (String category : categories) {
-            adapter.addFragment(ProductsListFragment.newInstance(ROYAL_ACTIVITY), category);
-        }
-        viewPager.setAdapter(adapter);
-    }
-
-    private Fragment createFragment(int fragmentToShow) {
-        Fragment fragment = null;
-        //Objects needed for Analytics tracking
+    private void createFragment(int fragmentToShow, ViewPager viewPager) {
         AnalyticEvent analyticEvent = new AnalyticEvent(AnalyticsConstants.KEY_FILTER_DISCOVER);
-        String categorySelected = "";
+        String categorySelected = null;
+        String idCategory = null;
+        int type = -1;
+
         switch (fragmentToShow) {
             case POSITION_ROYAL_ACTIVITY:
-                fragment = newInstance(ROYAL_ACTIVITY);
+                type = ROYAL_ACTIVITY;
                 categorySelected = getActivity().getString(R.string.royal_activity);
+                idCategory = ID_ACYIVITY;
                 break;
             case POSITION_DINING:
-                fragment = newInstance(DINING);
+                type = DINING;
                 categorySelected = getActivity().getString(R.string.dining);
+                idCategory = ID_DINING;
                 break;
             case POSITION_SHOPPING:
-                fragment = newInstance(SHOPPING);
+                type = SHOPPING;
                 categorySelected = getActivity().getString(R.string.shopping);
+                idCategory = ID_SHOPPING;
                 break;
             case POSITION_SPA:
-                fragment = newInstance(SPA);
+                type = SPA;
                 categorySelected = getActivity().getString(R.string.spa);
+                idCategory = ID_SPA;
                 break;
             case POSITION_SHOREX:
-                fragment = newInstance(SHOREX);
+                type = SHOREX;
                 categorySelected = getActivity().getString(R.string.shorex);
+                idCategory = ID_SHOREX;
                 break;
             case POSITION_ENTERTAINMENT:
-                fragment = newInstance(ENTERTAINMENT);
+                type = ENTERTAINMENT;
                 categorySelected = getActivity().getString(R.string.entertainment);
+                idCategory = ID_ENTRETAINMENT;
                 break;
             default:
                 Preconditions.unreachable();
         }
 
-        plansHeaderName.setText(categorySelected);
-        AnalyticsUtils.trackEvent(analyticEvent.addKeyValue(AnalyticsConstants.KEY_FILTER_CATEGORY, categorySelected));
-        return fragment;
-    }
-
-    private List<String> getCategoriesDummies(int fragmentToShow) {
-        List<String> categoriesDommies = new ArrayList<>();
-        switch (fragmentToShow) {
-            case POSITION_ROYAL_ACTIVITY:
-                categoriesDommies.add("Facial");
-                categoriesDommies.add("Hair");
-                categoriesDommies.add("Manicure");
-                break;
-            case POSITION_DINING:
-                break;
-            case POSITION_SHOPPING:
-                categoriesDommies.add("Message");
-                categoriesDommies.add("Facial");
-                break;
-            case POSITION_SPA:
-                categoriesDommies.add("Message");
-                categoriesDommies.add("Facial");
-                categoriesDommies.add("Hair");
-                categoriesDommies.add("Manicure");
-                break;
-            case POSITION_SHOREX:
-                categoriesDommies.add("Message");
-                break;
-            case POSITION_ENTERTAINMENT:
-                categoriesDommies.add("Message");
-                categoriesDommies.add("Facial");
-                categoriesDommies.add("Hair");
-                categoriesDommies.add("Manicure");
-                categoriesDommies.add("Manicure");
-                categoriesDommies.add("Manicure");
-                break;
-            default:
+        if (categorySelected != null && idCategory != null && type > -1) {
+            plansHeaderName.setText(categorySelected);
+            getSubCategoryDbUseCase = new GetSubCategoryDbUseCase(new SubCategoriesDataRepository());
+            subCategory = getSubCategoryDbUseCase.get(idCategory);
+            ProductsCategoryAdapter adapter = new ProductsCategoryAdapter(getFragmentManager());
+            List<ChildCategory> childCategories = subCategory.getChildCategory();
+            if (childCategories.isEmpty()) {
+                // TODO : No need tabs
+            } else {
+                for (int i = 0; i < childCategories.size(); i++) {
+                    adapter.addFragment(ProductsListFragment.newInstance(type), childCategories.get(i).getItems().getCategoryName());
+                }
+                viewPager.setAdapter(adapter);
+            }
+            AnalyticsUtils.trackEvent(analyticEvent.addKeyValue(AnalyticsConstants.KEY_FILTER_CATEGORY, categorySelected));
+        } else {
+            Timber.e("Don't find information for category");
         }
-        return categoriesDommies;
     }
 }
