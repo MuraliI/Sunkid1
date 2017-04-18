568a9325ff18aaf52a194a50021dc4b240856c1b
ads.f117 <ads.f117@gmail.com>
2017-04-18 17:18:53 -0500

    fix SaildDateInfoEntityDataMapperTest

--- excalibur/data/src/test/java/com/rcl/excalibur/data/mapper/SaildDateInfoEntityDataMapperTest.java
+++ excalibur/data/src/test/java/com/rcl/excalibur/data/mapper/SaildDateInfoEntityDataMapperTest.java
@@ -1,55 +1,37 @@
 package com.rcl.excalibur.data.mapper;
 
-import com.rcl.excalibur.data.entity.EventEntity;
-import com.rcl.excalibur.data.entity.ItineraryEntity;
-import com.rcl.excalibur.data.entity.PortEntity;
 import com.rcl.excalibur.data.entity.SailDateInfoEntity;
-import com.rcl.excalibur.data.service.response.PortResponse;
-import com.rcl.excalibur.data.service.response.SailDateEventResponse;
-import com.rcl.excalibur.domain.SailDateEvent;
 import com.rcl.excalibur.domain.SailDateInfoEvent;
-import com.rcl.excalibur.domain.SailDateItinerary;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class SaildDateInfoEntityDataMapperTest {
 
     private SaildDateInfoEntityDataMapper sailDateInfoDataMapper;
-
-    @Mock SailDateInfoEntity sailDateInfoEntity;
-    @Mock ItineraryEntity itineraryEntity;
-    @Mock List<EventEntity> eventEntities;
-
-    @Mock private EventEntity eventEntity1;
-    @Mock private EventEntity eventEntity2;
-
-    @Mock private PortEntity portEntity1;
-    @Mock private PortEntity portEntity2;
+    @Mock
+    SailDateInfoEntity sailDateInfoEntity;
 
     @Before
     public void setUp() throws Exception {
         sailDateInfoDataMapper = new SaildDateInfoEntityDataMapper();
         MockitoAnnotations.initMocks(this);
-        Mockito.when(sailDateInfoEntity.getDuration()).thenReturn(7);
-        Mockito.when(sailDateInfoEntity.getShipCode()).thenReturn("AL");
 
     }
 
     @Test
     public void transform() throws Exception {
+
         SailDateInfoEvent sailDateInfoEvent = sailDateInfoDataMapper.transform(sailDateInfoEntity);
         assertNotNull(sailDateInfoEvent);
         assertEquals(sailDateInfoEvent.getDuration(), sailDateInfoEntity.getDuration());
         assertEquals(sailDateInfoEvent.getShipCode(), sailDateInfoEntity.getShipCode());
-
+        assertEquals(sailDateInfoEvent.getItinerary(), sailDateInfoEntity.getItinerary());
 
     }
 
