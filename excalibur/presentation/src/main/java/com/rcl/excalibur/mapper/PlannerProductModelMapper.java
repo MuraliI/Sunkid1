3286dbb59789f7c9f0773c0be49ffbfeab3c05a7
GLOBANT\diego.recalde <diego.recalde@globant.com>
2017-04-26 16:34:08 -0500

    Changed condition to ask for hours instead of quantity of offerings.
    JIRA: None
    Signed-off-by:
    @difereto_globant <diego.recalde@globant.com>

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mapper/PlannerProductModelMapper.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mapper/PlannerProductModelMapper.java
@@ -28,7 +28,10 @@ public class PlannerProductModelMapper {
     public static final int TIMED_PRODUCT_LIST = 1;
 
     private static final int ALL_DAY_OFFERING_LIMIT = 10;
+    private static final int ALL_DAY_DURATION_IN_MINUTES = 300;
     private static final int DEFAULT_DURATION = 0;
+    private static final int FIRST_OFFERING = 0;
+    private static final int INITIAL_CAPACITY = 2;
 
     private BaseProductInformationMapper productInformationMapper;
     private Resources resources;
@@ -41,22 +44,37 @@ public class PlannerProductModelMapper {
     public SparseArrayCompat<List<PlannerProductModel>> transform(List<Offering> input) {
         List<PlannerProductModel> timedProductList = new ArrayList<>();
         List<PlannerProductModel> allDayProducts = new ArrayList<>();
-        SparseArrayCompat<List<PlannerProductModel>> productList = new SparseArrayCompat<>(2);
+
+        SparseArrayCompat<List<PlannerProductModel>> productList = new SparseArrayCompat<>(INITIAL_CAPACITY);
         productList.append(ALL_DAY_PRODUCT_LIST, allDayProducts);
         productList.append(TIMED_PRODUCT_LIST, timedProductList);
+
         Map<String, List<Offering>> productOfferingMap = createOfferingProductMap(input);
         for (Map.Entry<String, List<Offering>> entry : productOfferingMap.entrySet()) {
             List<Offering> productOfferingList = entry.getValue();
-            if (productOfferingList.size() >= ALL_DAY_OFFERING_LIMIT) {
+
+            long productDuration = productOfferingList.get(FIRST_OFFERING).getProduct()
+                    .getProductDuration().getDurationInMinutes();
+            if (productDuration >= ALL_DAY_DURATION_IN_MINUTES) {
                 allDayProducts.add(createAllDayProductModel(productOfferingList));
             } else {
                 for (Offering offering : productOfferingList) {
                     timedProductList.add(createNormalPlannerProductModel(offering));
                 }
             }
+
+           /* if (productOfferingList.size() >= ALL_DAY_OFFERING_LIMIT) {
+                allDayProducts.add(createAllDayProductModel(productOfferingList));
+            } else {
+                for (Offering offering : productOfferingList) {
+                    timedProductList.add(createNormalPlannerProductModel(offering));
+                }
+            }*/
         }
+
         Collections.sort(allDayProducts);
         Collections.sort(timedProductList);
+
         return productList;
     }
 
@@ -78,18 +96,19 @@ public class PlannerProductModelMapper {
 
     private PlannerProductModel createAllDayProductModel(@NonNull List<Offering> offeringList) {
         Collections.sort(offeringList);
-        Product product = offeringList.get(0).getProduct();
+
+        Product product = offeringList.get(FIRST_OFFERING).getProduct();
         PlannerProductModel model = (PlannerProductModel) productInformationMapper.transform(product);
 
         Calendar allDayStartDate = Calendar.getInstance();
-        allDayStartDate.setTime(offeringList.get(0).getDate());
+        allDayStartDate.setTime(offeringList.get(FIRST_OFFERING).getDate());
 
-        Calendar allDayEndDate = Calendar.getInstance();
-        allDayEndDate.setTime(offeringList.get(offeringList.size() - 1).getDate());
+        Calendar allDayEndDate = calculateEndDate(offeringList.get(offeringList.size() - 1).getDate(),
+                product.getProductDuration());
 
-        model.setOperatingHours(calculateOperatingHours(allDayStartDate, allDayEndDate));
         model.setStartDate(allDayStartDate);
         model.setEndDate(allDayEndDate);
+        model.setOperatingHours(calculateOperatingHours(model.getStartDate(), model.getEndDate()));
         model.setHighlighted(product.isHighlighted());
         model.setFeatured(product.isFeatured());
         model.setAllDayProduct(true);
@@ -110,9 +129,7 @@ public class PlannerProductModelMapper {
         model.setStartDate(startDate);
         model.setEndDate(calculateEndDate(offering.getDate(), product.getProductDuration()));
         model.setOperatingHours(calculateOperatingHours(model.getStartDate(), model.getEndDate()));
-        if (model.getProductType() != null) {
-            model.setResourceIdCategoryIcon(CategoryUtils.getCategoryIcon(model.getProductType()));
-        }
+        model.setResourceIdCategoryIcon(CategoryUtils.getCategoryIcon(model.getProductType()));
         model.setLocation(resources.getString(R.string.deck_label) + ConstantsUtil.WHITE_SPACE + model.getLocation());
         model.setFeatured(product.isFeatured());
         model.setHighlighted(product.isHighlighted());
