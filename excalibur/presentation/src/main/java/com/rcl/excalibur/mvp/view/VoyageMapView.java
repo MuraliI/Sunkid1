78b1c0ff95407d3543594fbedde9205ad5224dc8
GLOBANT\gerardo.suarez <gerardo.suarez@globant.com>
2017-05-03 11:48:56 -0500

    set text of the header

--- excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/VoyageMapView.java
+++ excalibur/presentation/src/main/java/com/rcl/excalibur/mvp/view/VoyageMapView.java
@@ -98,6 +98,6 @@ public class VoyageMapView extends ActivityView<VoyageMapActivity, Void, Void> {
 
     public void setTextShipLocation(String textShip, String day) {
         dayPickerText.setText(day);
-        textShipText.setText(textShip);
+        textShipText.setText(textShips);
     }
 }
